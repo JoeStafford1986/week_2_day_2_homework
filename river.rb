10252378fe2ac507bb1d8945018be0414cec8b10
Joe Stafford <joestafford1986@gmail.com>
2018-04-17 17:28:49 +0100

    added fish array and populated with fish

--- river.rb
+++ river.rb
@@ -2,9 +2,9 @@ class River
 
   attr_reader(:name)
 
-  def initialize(name)
+  def initialize(name, fish)
     @name = name
-    @fish = []
+    @fish = fish
   end
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -1,12 +1,14 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../river.rb")
+require_relative("../fish.rb")
 
 
 class RiverTest < MiniTest::Test
 
   def setup
-    @river = River.new("Amazon")
+    fish = [Fish.new("Sean"), Fish.new("Pauline"), Fish.new("Marcus"), Fish.new("Craig"), Fish.new("Edith")]
+    @river = River.new("Amazon", fish)
   end
 
   def test_get_name
